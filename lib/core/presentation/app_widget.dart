b0848f5922c1bf03e647856ced1d4a6952f8f1d5
muchlis <whois.muchlis@gmail.com>
2022-02-08 15:56:32 +0800

    implement auth usecase

--- lib/core/presentation/app_widget.dart
+++ lib/core/presentation/app_widget.dart
@@ -1,6 +1,15 @@
-
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
+import 'package:github_view/auth/application/auth_notifier.dart';
+import 'package:github_view/auth/shared/providers.dart';
 import 'package:github_view/core/presentation/routes/app_router.gr.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final initializationProvider = FutureProvider<Unit>((ref) async {
+  final authNotifier = ref.watch(authNotifierProvider.notifier);
+  await authNotifier.checkAndUpdateAuthStatus();
+  return unit;
+});
 
 class AppWidget extends StatelessWidget {
   final appRouter = AppRouter();
@@ -9,10 +18,33 @@ class AppWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Github Viewer',
-      routerDelegate: appRouter.delegate(),
-      routeInformationParser: appRouter.defaultRouteParser(),
+    return ProviderListener(
+      provider: initializationProvider,
+      onChange: (context, _, __) {},
+      child: ProviderListener<AuthState>(
+        provider: authNotifierProvider,
+        onChange: (context, _, state) {
+          state.maybeMap(
+              orElse: () {},
+              authenticated: (_) {
+                appRouter.pushAndPopUntil(
+                  const StarredReposRoute(),
+                  predicate: (route) => false,
+                );
+              },
+              unauthenticated: (_) {
+                appRouter.pushAndPopUntil(
+                  const SignInRoute(),
+                  predicate: (route) => false,
+                );
+              });
+        },
+        child: MaterialApp.router(
+          title: 'Repo Viewer',
+          routerDelegate: appRouter.delegate(),
+          routeInformationParser: appRouter.defaultRouteParser(),
+        ),
+      ),
     );
   }
 }
--- lib/starred_repos/presentation/starred_repos_page.dart
+++ lib/starred_repos/presentation/starred_repos_page.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:github_view/auth/shared/providers.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class StarredReposPage extends StatelessWidget {
-  const StarredReposPage({ Key? key }) : super(key: key);
+class StarredReposPage extends ConsumerWidget {
+  const StarredReposPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            ref.read(authNotifierProvider.notifier).signOut();
+          },
+          child: const Text("signOut"),
+        ),
+      ),
     );
   }
 }
